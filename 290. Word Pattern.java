3647418fe30357f521a0d0ea3ba10d5637990f30
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-01-03 15:21:45 -0500

    adding optimised appraoch

--- 290. Word Pattern.java
+++ 290. Word Pattern.java
@@ -35,4 +35,26 @@ class Solution {
     }
 }
 
-//OPTIMAL
+//OPTIMISED APPROACH
+
+HashMap map_index = new HashMap();
+String[] words = s.split(" ");
+
+if (words.length != pattern.length())
+    return false;
+
+for (Integer i = 0; i < words.length; i++) {
+    char c = pattern.charAt(i);
+    String w = words[i];
+
+    if (!map_index.containsKey(c))
+        map_index.put(c, i);
+
+    if (!map_index.containsKey(w))
+        map_index.put(w, i);
+
+    if (map_index.get(c) != map_index.get(w))
+        return false;
+}
+
+return true;
