6b8ef948f856078e9bc3af03cc4b8d94c70c82f3
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-01-09 00:20:19 -0700

    contains duplicate

---
+++ 217. Contains Duplicate.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        //BRUTE FORCE
+        /*ArrayList<Integer> arr =new ArrayList<>();
+        for(int i:nums)
+        {
+            if(arr.contains(i))
+            {
+                return true;
+            }
+            else
+            {
+                arr.add(i);
+            }
+        }
+
+        return false;
+
+*/
+
+        //HASHSET
+        HashSet<Integer> set = new HashSet<>();
+        for(int i:nums)
+        {
+            set.add(i);
+        }
+        if(set.size()==nums.length)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+}
