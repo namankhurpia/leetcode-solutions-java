764f72455a0a8dfd42c457fb94ec09bdc7cf93b8
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-01-22 15:46:34 -0500

    adding

---
+++ 1920. Build Array from Permutation.java
@@ -0,0 +1,12 @@
+//OPTIMISED SOLUTION - O(n)
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int arr[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            arr[i] = nums[nums[i]]; 
+        }
+        return arr;
+
+    }
+}
