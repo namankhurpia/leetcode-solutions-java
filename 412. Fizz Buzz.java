a082490eff52630e6f44b66c62a99e4c0d5a4cba
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-01-26 13:56:08 -0500

    add

---
+++ 412. Fizz Buzz.java
@@ -0,0 +1,30 @@
+//OPTIMAL SOLUTION
+class Solution {
+    public List<String> fizzBuzz(int n) {
+
+        ArrayList<String> res = new ArrayList<String>();
+        for(int i=1;i<=n;i++)
+        {
+            //System.out.println("I:"+i);
+            if(i%5==0 && i%3==0)
+            {
+                res.add("FizzBuzz");
+            }   
+            else if(i%5==0)
+            {
+                res.add("Buzz");
+            }
+            else if(i%3==0)
+            {
+                res.add("Fizz");
+            }
+            else
+            {
+                res.add(i+"");
+            }
+        }
+        return res;
+        
+        
+    }
+}
