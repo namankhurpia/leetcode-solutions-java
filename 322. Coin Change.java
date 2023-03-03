28376a02515ab3287d83fc4de768a3a3c80d43a2
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-03-03 18:52:33 -0500

    Add

---
+++ 322. Coin Change.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+        int arr[] = new int[amount+1];
+        Arrays.fill(arr,amount+1);
+        
+        arr[0]=0;
+
+        for(int i=1;i<=amount;i++)
+        {
+            for(int j=0;j<coins.length;j++)
+            {
+                if(coins[j]<=i)
+                {
+                    arr[i] = Math.min(arr[i], arr[i-coins[j]] + 1);
+                }
+                
+            }
+        }
+        return arr[amount]>amount? -1: arr[amount];
+
+    }
+}
