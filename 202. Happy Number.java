2030e2be79bef359071972b764ce6873f6c13b46
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-03-03 17:47:00 -0500

    adding more ways to solve problem

--- 202. Happy Number.java
+++ 202. Happy Number.java
@@ -38,4 +38,71 @@ class Solution {
     }
 }
 
-//optimal approach 
+//optimal approach -1
+//Floyd's cycle detection algorithm
+class Solution {
+    public boolean isHappy(int n) {
+
+ 
+         int slow=n,fast=getsquaresum(n);
+         while(n!=1 && slow!=fast)
+         {
+             slow = getsquaresum(slow);
+             fast = getsquaresum(getsquaresum(fast));
+             System.out.println("slow is:"+slow+" fast is:"+fast);
+         }
+         if(slow==1 || fast==1)
+         {
+             return true;
+         }
+         return false;
+         
+ 
+     }
+ 
+     public static int getsquaresum(int n)
+     {
+         int sumsquare = 0;
+         while(n!=0)
+         {
+             sumsquare = sumsquare + (n%10)*(n%10);
+             n = n/10;
+         }
+         return sumsquare;
+     }
+ }
+
+
+ //FASTEST APPROACH
+//hardcoding the only cycle available
+ class Solution {
+    public boolean isHappy(int n) {
+
+         Set<Integer> cycle = new HashSet<Integer>(Arrays.asList(4,16,37,58,89,145,42,20));
+         while (n != 1 && !cycle.contains(n))
+         {
+             n = getsquaresum(n);
+         }
+         if(n == 1 )
+         {
+             return true;
+         }
+         else
+         {   
+             return false;
+         }
+         
+ 
+     }
+ 
+     public static int getsquaresum(int n)
+     {
+         int sumsquare = 0;
+         while(n!=0)
+         {
+             sumsquare = sumsquare + (n%10)*(n%10);
+             n = n/10;
+         }
+         return sumsquare;
+     }
+ }
