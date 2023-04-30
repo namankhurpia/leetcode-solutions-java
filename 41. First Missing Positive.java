7fef09402f0313d02ee4c1ee56f69aa8c6e4c212
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-04-30 17:44:35 -0400

    add

--- 41. First Missing Positive.java
+++ 41. First Missing Positive.java
@@ -1,4 +1,4 @@
-//brute force solution
+//brute force solution - O(n) but space complexity is high
 
 class Solution {
     public int firstMissingPositive(int[] nums) {
@@ -42,3 +42,61 @@ class Solution {
         
     }
 }
+
+//OPTIMAL SOLUTION - O(n)
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        int n = nums.length;
+
+        int c =0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==1)
+            {
+                c++;
+            }
+        }
+        if(c==0)
+            return 1;
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]<=0 || nums[i]>n)
+            {
+                nums[i] = 1;
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            int a = Math.abs(nums[i]);
+            if(a==n)
+            {
+                nums[0] = - Math.abs(nums[0]);
+            }
+            else
+            {
+                nums[a] = - Math.abs(nums[a]);
+            }
+        }
+
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]>0)
+                return i;
+        }
+
+        if(nums[0]>0)
+            return n;
+        
+        return n+1;
+
+
+
+
+        
+    }
+}
+
