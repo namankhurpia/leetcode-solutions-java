160b4baee7401a91bb7dabb221d8926837a27ca2
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-06-15 18:06:22 +0530

    add

---
+++ 28. Find the Index of the First Occurrence in a String.java
@@ -0,0 +1,35 @@
+// optimal solution - O(n)
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+   if(needle.length()>haystack.length())
+   {
+       return -1;
+   }
+
+    int hl = haystack.length();
+    haystack+=needle;
+    int ln = needle.length();
+    for(int i=0;i<hl;i++)
+    {
+        String check = haystack.substring(i,i+ln);
+        if(needle.equalsIgnoreCase(check))
+        {
+            return i;
+        }
+    }
+    return -1;
+   
+
+    }
+}
+
+//one liner solution
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+    return  haystack.indexOf(needle);
+
+    }
+}
