e950af2770aa080562a20f21e6294b59fe7cb71a
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-07-03 13:55:58 +0530

    add

--- 268. Missing Number.java
+++ 268. Missing Number.java
@@ -14,7 +14,7 @@ class Solution {
                 return i;
             }
         }
-        return len;
+        return len`;
 
     }
 }
--- 287. Find the Duplicate Number.java
+++ 287. Find the Duplicate Number.java
@@ -37,3 +37,13 @@ class Solution {
         
     }
 }
+
+
+//optimal approach        
+        while (nums[0] != nums[nums[0]]) {
+            int nxt = nums[nums[0]];
+            nums[nums[0]] = nums[0];
+            nums[0] = nxt;
+        }
+        return nums[0];
+    
