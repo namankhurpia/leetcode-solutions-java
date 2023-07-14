140a878e160e9dcd4b3d8c077fdc7938cea3cb35
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-07-14 10:17:42 +0530

    add

---
+++ GFG Minimum Plateforms.java
@@ -0,0 +1,38 @@
+//Minimum Platforms
+//https://practice.geeksforgeeks.org/problems/minimum-platforms-1587115620/1#
+
+class Solution
+{
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        // add your code here
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        
+        int i=1,j =0;
+        int pla =1;
+        int res =0;
+        
+        while( i<n && j<n)
+        {
+            if(arr[i]<=dep[j])
+            {
+                pla ++;
+                i++;
+            }
+            
+            else if(arr[i]> dep[j])
+            {
+                pla--;
+                j++;
+            }
+            
+            res = Math.max(res,pla);
+        }
+        return res;
+        
+    }
+    
+}
