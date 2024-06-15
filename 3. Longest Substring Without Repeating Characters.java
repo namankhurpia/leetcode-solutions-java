2b66a887e541539e9f3d4a62eee08d19db9a0a97
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2024-06-15 12:09:10 -0400

    add

--- 3. Longest Substring Without Repeating Characters.java
+++ 3. Longest Substring Without Repeating Characters.java
@@ -21,3 +21,32 @@ class Solution {
 
       
 }
+
+
+
+//optimal again - jun 15 2024
+// approach - consider 2 pointers left and right for the string s, now create a set which contains character.
+// this set will contains unique characters of string s, keep moving from left to right and everytime you cencounter a unique character add it to set, but if it exists remove it using a loop to constantly update the set
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+        int left = 0;
+        int right = s.length();
+        int max = 0;
+        ArrayList<Character> set = new ArrayList<>();
+
+        for(left=0;left<s.length();left++)
+        {
+            while(set.contains(Character.valueOf(s.charAt(left))))
+            {
+                set.remove(0);
+            }
+            
+                set.add(Character.valueOf(s.charAt(left)));
+                max = Math.max(set.size(),max);
+            
+        }
+        return max;
+
+    }
+}
