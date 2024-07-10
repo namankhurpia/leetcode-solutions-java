a262782a1eb51117d8ad64eb0c945324121fb6f7
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2024-07-09 23:24:12 -0500

    add

--- 706. Design HashMap.java
+++ 706. Design HashMap.java
@@ -29,7 +29,7 @@ class MyHashMap {
             }
 
             values.set(pos, value);
-            
+
         }
         
         
@@ -90,4 +90,28 @@ class MyHashMap {
  * obj.remove(key);
  */
 
- 
+ class MyHashMap {
+
+   int[] data;
+    public MyHashMap() {
+        data = new int[1000001];
+        Arrays.fill(data, -1);
+    }
+    public void put(int key, int val) {
+        data[key] = val;
+    }
+    public int get(int key) {
+        return data[key];
+    }
+    public void remove(int key) {
+        data[key] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
