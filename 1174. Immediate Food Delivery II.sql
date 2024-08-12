15fc356794bdb87fec99df6ddaba7948870e7a64
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2024-08-11 17:03:33 -0700

    add

--- 1174. Immediate Food Delivery II.sql
+++ 1174. Immediate Food Delivery II.sql
@@ -55,6 +55,10 @@ Hence, half the customers have immediate first orders.
 
 # Write your MySQL query statement below
 
-select ROUND(AVG(IF(order_date = customer_pref_delivery_date ,1,0)*100),2) as immediate_percentage 
-from Delivery where (customer_id, order_date) in
-(select customer_id, min(order_date) as first_order from Delivery GROUP BY customer_id); 
+select ROUND(AVG(order_date = customer_pref_delivery_date)*100,2) as 'immediate_percentage'
+from Delivery
+where (customer_id, order_date) in (
+    select customer_id, min(order_date)
+    from Delivery
+    group by customer_id
+)
